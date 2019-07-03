1cdae0a2aa17dfa4cc767bbb02972fe23944323b
凌 <myywlc@163.com>
2019-07-03 16:56:36 +0800

    fix api

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,10 +1,23 @@
 const Router = require('koa-router');
-const router = new Router();
+const { HotBook } = require('../../models/hot-book');
+const { Book } = require('../../models/book');
+const { PositiveIntegerValidator } = require('../../validators/validator');
 
-router.get('/v1/book/latest', (ctx, next) => {
+const router = new Router({
+  prefix: '/v1/book',
+});
+
+router.get('/hot_list', async (ctx, next) => {
+  const books = await HotBook.getAll();
   ctx.body = {
-    key: 'book',
+    books,
   };
+});
+
+router.get('/:id/detail', async ctx => {
+  const v = await new PositiveIntegerValidator().validate(ctx);
+  const book = new Book(v.get('path.id'));
+  ctx.body = await book.detail();
 });
 
 module.exports = router;
---
+++ app/models/hot-book.js
@@ -0,0 +1,56 @@
+const { sequelize } = require('../../core/db');
+const { Sequelize, Model, Op } = require('sequelize');
+const { Favor } = require('./favor');
+
+class HotBook extends Model {
+  static async getAll() {
+    const books = await HotBook.findAll({
+      order: [
+        'index',
+      ],
+    });
+    const ids = [];
+    books.forEach((book) => {
+      ids.push(book.id);
+    });
+    const favors = await Favor.findAll({
+      where: {
+        art_id: {
+          [Op.in]: ids,
+        },
+        type: 400,
+      },
+      group: ['art_id'],
+      attributes: ['art_id', [Sequelize.fn('COUNT', '*'), 'count']],
+    });
+    books.forEach(book => {
+      HotBook._getEachBookStatus(book, favors);
+    });
+    return books;
+  }
+
+  static _getEachBookStatus(book, favors) {
+    let count = 0;
+    favors.forEach(favor => {
+      if (book.id === favor.art_id) {
+        count = favor.get('count');
+      }
+    });
+    book.setDataValue('count', count);
+    return book;
+  }
+}
+
+HotBook.init({
+  index: Sequelize.INTEGER,
+  image: Sequelize.STRING,
+  author: Sequelize.STRING,
+  title: Sequelize.STRING,
+}, {
+  sequelize,
+  tableName: 'hot_book',
+});
+
+module.exports = {
+  HotBook,
+};
