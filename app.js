105b67c8665d511b13e0f1224f931548125e00d2
凌 <myywlc@163.com>
2019-07-07 02:18:16 +0800

    fix api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('module-alias/register');
 const Koa = require('koa');
 const parser = require('koa-bodyparser'); // 对于POST请求的处理,把koa2上下文的formData数据解析到ctx.request.body中
 const static = require('koa-static');
+const logger = require('koa-logger');
 const path = require('path');
 const InitManager = require('./core/init');
 const catchError = require('./middlewares/exception');
@@ -11,6 +12,7 @@ const catchError = require('./middlewares/exception');
 const app = new Koa();
 app.use(catchError);
 app.use(parser());
+app.use(logger());
 app.use(static(path.join(__dirname, './static')));
 
 InitManager.initCore(app);
