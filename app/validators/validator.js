44ffb8c75e85e174105d4b657b822b48d3b07be6
凌 <myywlc@163.com>
2019-08-08 01:13:43 +0800

    fix

--- app/validators/validator.js
+++ app/validators/validator.js
@@ -179,7 +179,7 @@ class SearchValidator extends LinValidator {
 
 class AddShortCommentValidator extends PositiveIntegerValidator {
   constructor() {
-    super()
+    super();
     this.content = [
       new Rule('isLength', '必须在1到12个字符之间', {
         min: 1,
