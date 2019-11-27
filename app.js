0d3d4414bc32e3a075e53f1e4e18253fc40fa58a
凌 <myywlc@163.com>
2019-11-27 22:51:44 +0800

    移除 module-alias

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const Koa = require('koa');
+const path = require('path');
 const parser = require('koa-bodyparser'); // 对于POST请求的处理,把koa2上下文的formData数据解析到ctx.request.body中
 const statics = require('koa-static');
 const logger = require('koa-logger');
-const path = require('path');
-const InitManager = require('./core/init');
 const catchError = require('./middlewares/exception');
+const InitManager = require('./core/init');
+
 
 const app = new Koa();
 app.use(logger());
