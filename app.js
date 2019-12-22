fcf80e9f7d4a6a0b42c44e43e3f30c18704c84ad
凌 <myywlc@163.com>
2019-12-22 20:19:34 +0800

    fix remove moment add dayjs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const parser = require('koa-bodyparser'); // 对于POST请求的处理,把koa2上下文的formData数据解析到ctx.request.body中
 const statics = require('koa-static');
 const logger = require('koa-logger');
-const moment = require('moment');
+const dayjs = require('dayjs');
 const catchError = require('./middlewares/exception');
 const InitManager = require('./core/init');
 
@@ -11,7 +11,7 @@ const app = new Koa();
 
 app.use(
   logger(str => {
-    console.log(`${moment().format('YYYY-MM-DD HH:mm:ss.SSS')} ${str}`);
+    console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss.SSS')} ${str}`);
   }),
 );
 app.use(catchError);
