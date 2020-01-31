e3b48b74d298461555e716c893327f4d1c6929d9
凌 <myywlc@163.com>
2020-01-31 14:19:43 +0800

    fix loadKoaMiddleware

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
 const Koa = require('koa');
-const path = require('path');
-const parser = require('koa-bodyparser'); // 对于POST请求的处理,把koa2上下文的formData数据解析到ctx.request.body中
-const statics = require('koa-static');
-const logger = require('koa-logger');
-const dayjs = require('dayjs');
-const catchError = require('./middlewares/exception');
 const InitManager = require('./core/init');
 
 const app = new Koa();
-
-app.use(
-  logger(str => {
-    console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss.SSS')} ${str}`);
-  }),
-);
-app.use(catchError);
-app.use(parser());
-app.use(statics(path.join(__dirname, './static')));
-
 InitManager.initCore(app);
 
 app.listen(3000, () => {
--- core/init.js
+++ core/init.js
@@ -1,3 +1,9 @@
+const path = require('path');
+const parser = require('koa-bodyparser'); // 对于POST请求的处理,把koa2上下文的formData数据解析到ctx.request.body中
+const statics = require('koa-static');
+const logger = require('koa-logger');
+const dayjs = require('dayjs');
+const catchError = require('../middlewares/exception');
 const requireDirectory = require('require-directory');
 const Router = require('koa-router');
 
@@ -5,9 +11,21 @@ class InitManager {
   static initCore(app) {
     //入口方法
     InitManager.app = app;
+    InitManager.loadKoaMiddleware();
     InitManager.initLoadRouters();
     InitManager.loadHttpException();
     InitManager.loadConfig();
+  }
+
+  static loadKoaMiddleware() {
+    InitManager.app.use(
+      logger(str => {
+        console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss.SSS')} ${str}`);
+      }),
+    );
+    InitManager.app.use(catchError);
+    InitManager.app.use(parser());
+    InitManager.app.use(statics(path.join(process.cwd(), './static')));
   }
 
   static loadConfig(path = '') {
